72f3d7e176acdd7f2f64de59fd5b0d02b501cdd0
adreider <adreidereconomic@gmail.com>
2021-08-10 17:52:50 -0300

    add color loading

--- landing.js
+++ landing.js
@@ -1,19 +1,19 @@
-const Landing = {
-	open() {
-		// Abrir Modal
-		// Adicionar a classe active ao Modal
-		document.getElementById('modal-ls').classList.add('active');
-	},
-	close() {
-		// Fechar o Modal
-		//Remover a classe active do Modal
-		document.querySelector('.modal-overlay').classList.remove('active');
-	},
-};
+// const Landing = {
+// 	open() {
+// 		// Abrir Modal
+// 		// Adicionar a classe active ao Modal
+// 		document.getElementById('modal-ls').classList.add('active');
+// 	},
+// 	close() {
+// 		// Fechar o Modal
+// 		//Remover a classe active do Modal
+// 		document.querySelector('.modal-overlay').classList.remove('active');
+// 	},
+// };
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  Landing.open();
-  setTimeout(() => {
-    Landing.close();
-  }, 5000)
-});
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   Landing.open();
+//   setTimeout(() => {
+//     Landing.close();
+//   }, 5000)
+// });
