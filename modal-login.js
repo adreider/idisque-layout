0398e9ba8bea78d8e0f4521aa3286b0e97f74a9a
adreider <adreidereconomic@gmail.com>
2021-08-10 17:28:07 -0300

    update loading

--- modal-login.js
+++ modal-login.js
@@ -1,12 +1,12 @@
-const Login = {
-	open() {
-		// Abrir Modal
-		// Adicionar a classe active ao Modal
-		document.getElementById('modal-login').classList.add('active');
-	},
-	close() {
-		// Fechar o Modal
-		//Remover a classe active do Modal
-		document.querySelector('.modal-login-overlay').classList.remove('active');
-	},
-};
+// const Login = {
+// 	open() {
+// 		// Abrir Modal
+// 		// Adicionar a classe active ao Modal
+// 		document.getElementById('modal-login').classList.add('active');
+// 	},
+// 	close() {
+// 		// Fechar o Modal
+// 		//Remover a classe active do Modal
+// 		document.querySelector('.modal-login-overlay').classList.remove('active');
+// 	},
+// };
